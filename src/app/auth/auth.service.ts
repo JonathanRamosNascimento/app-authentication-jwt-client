30bf9ae20a46788ee92251e136f7e38602f3e46f
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-15 01:09:45 -0300

    storing the API generated token in the application's local storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { User } from './user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,9 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   readonly url = 'http://localhost:3000/auth';
+
+  private subjUser$: BehaviorSubject<User> = new BehaviorSubject(null);
+  private subjLoggedIn$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor(
     private http: HttpClient
@@ -19,6 +23,22 @@ export class AuthService {
   }
 
   login(credentials: { email: string, password: string }): Observable<User> {
-    return this.http.post<User>(`${this.url}/login`, credentials);
+    return this.http
+      .post<User>(`${this.url}/login`, credentials)
+      .pipe(
+        tap((u: User) => {
+          localStorage.setItem('token', u.token);
+          this.subjLoggedIn$.next(true);
+          this.subjUser$.next(u);
+        })
+      )
+  }
+
+  isAuthenticated(): Observable<boolean> {
+    return this.subjLoggedIn$.asObservable();
+  }
+
+  getUser(): Observable<User> {
+    return this.subjUser$.asObservable();
   }
 }
