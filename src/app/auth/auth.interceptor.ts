0a2dba4ec47f48d92527ab0f625ee3fdb79df73e
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-17 16:30:05 -0300

    Intercepting HTTP Requests

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    constructor() {
+
+    }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        console.log(req);
+        return next.handle(req);
+    }
+}
